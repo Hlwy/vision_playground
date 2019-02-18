28b04e4c610537353471668c2acc946ba469147e
Hunter Young <hunter.lw.young@gmail.com>
2019-02-18 01:21:37 -0500

    obstacle detection

--- src/UVMappingPipeline.py
+++ src/UVMappingPipeline.py
@@ -15,19 +15,25 @@ class UVMappingPipeline:
         self.ground_line = []
         self.ground_pts = []
 
+        # Obstacle Information
+        self.xBounds = []
+        self.disparityBounds = []
+
         # Filtered Map Images
         self.greyU = []; self.closingU = []; self.dilatedU = []; self.blurredU = []
         self.greyV = []; self.closingV = []; self.dilatedV = []; self.blurredV = []
 
         # Other Useful Information
-        self.contours = []
-        self.contour_hierarchy = []
+        self.contours = self.contoursU = self.contoursV = []
+        self.contour_hierarchy = self.contour_hierarchyU = self.contour_hierarchyV = []
 
         # Previously Used Values (Helps prevent redundant computations)
         self.prev_img_path = None
         self.flag_new_img = False
         self.prev_map_flag = False
-        self.prev_e1, self.prev_e2, self.prev_grey_thresh = 0, 0, 0
+        self.prev_e1, self.prev_e2 = 0, 0
+        self.prev_grey_threshU =  0
+        self.prev_grey_threshV =  0
 
         # Sizes
         self.h = self.hv= 480
@@ -35,6 +41,8 @@ class UVMappingPipeline:
         self.dmax = self.hu = self.wv = 256
         self.window_size = [10,30]
         self.mask_size = [20,40]
+        self.deadzone_vd = 4
+        self.deadzone_ud = 3
 
         # Counters
         self.nPlots = 0
@@ -46,9 +54,9 @@ class UVMappingPipeline:
     """
     def pipeline(self, _img="test/test_disparity.png",
         line_method = 1, input_method = 0,
-        e1 = 2, e2 = 5, ang = 90, rho = 1, minLineLength = 11, maxLineGap = 11,
-        houghThresh = 50, greyThresh = 11, cnt_thresh = 500.0,
-        show_helpers = True, use_umap = True, flip_thresh_bin = False
+        e1 = 1, e2 = 5, ang = 90, rho = 1, minLineLength = 11, maxLineGap = 11,
+        houghThresh = 50, greyThreshU = 35, greyThreshV=14, cnt_thresh = 50.0,
+        show_helpers = True, use_umap = False, flip_thresh_bin = False
         ):
 
         # Parse inputs to create string descriptors for control variable copying
@@ -64,7 +72,8 @@ class UVMappingPipeline:
 
         # Check variable arguments for changes to reduce repeating unnecessary calculations
         if((use_umap is not self.prev_map_flag) or (self.flag_new_img) or
-           (greyThresh is not self.prev_grey_thresh) or
+           (greyThreshU is not self.prev_grey_threshU) or
+           (greyThreshV is not self.prev_grey_threshV) or
            (e1 is not self.prev_e1) or (e2 is not self.prev_e2)
         ):
             flag_needs_filtering = True
@@ -75,7 +84,8 @@ class UVMappingPipeline:
 
         # Store current control paramters for next iteration
         self.prev_map_flag = use_umap
-        self.prev_grey_thresh = greyThresh
+        self.prev_grey_threshU = greyThreshU
+        self.prev_grey_threshV = greyThreshV
         self.prev_e1, self.prev_e2 = e1, e2
 
         print(
@@ -83,18 +93,19 @@ class UVMappingPipeline:
         Inputs:  (image = %s)
         ------
 
-        \t* Mapping Space       : %s
-        \t* Line Finding Method : %s
-        \t* Filtering Used      : %s
-        \t* Kernel Size         : (%d, %d)
-        \t* Rho, Angle (deg)    :  %d, %d
-        \t* Min Line Length     :  %d
-        \t* Max Line Gap        :  %d
-        \t* Grey Thresholding   :  %d
-        \t* Hough Threshold     :  %d
-        \t* Contour Threshold   :  %d
+        \t* Mapping Space               : %s
+        \t* Line Finding Method         : %s
+        \t* Filtering Used              : %s
+        \t* Kernel Size                 : (%d, %d)
+        \t* Rho, Angle (deg)            :  %d, %d
+        \t* Min Line Length             :  %d
+        \t* Max Line Gap                :  %d
+        \t* Grey Thresholding (U-Map)   :  %d
+        \t* Grey Thresholding (V-Map)   :  %d
+        \t* Hough Threshold             :  %d
+        \t* Contour Threshold           :  %d
         """ % (
-            _img,strMapSpace,strLineMeth,strFilterMeth,e1,e2,rho,ang,minLineLength,maxLineGap,greyThresh,houghThresh,cnt_thresh
+            _img,strMapSpace,strLineMeth,strFilterMeth,e1,e2,rho,ang,minLineLength,maxLineGap,greyThreshU,greyThreshV,houghThresh,cnt_thresh
         ))
         # Convert Angle to radians for calculations
         ang = ang * np.pi/180
@@ -103,9 +114,35 @@ class UVMappingPipeline:
         self.read_image(_img)
         self.get_uv_map()
 
-        self.filter_image(greyThresh, e1, e2, timing=True)
-        cnts, hier = self.find_contours(self.greyV,cnt_thresh)
+        self.plot_image(self.overlay,0)
+
+        self.filter_image(greyThreshU, greyThreshV, e1, e2, timing=True)
+        if(use_umap): cntImgU = self.greyU
+        else: cntImgU = self.dilatedU
+
+        # cntsV, hierV = self.find_contours(self.greyV,cnt_thresh)
+        # self.contoursV = cntsV; self.contour_hierarchyV = hierV
+        cntsU, hierU = self.find_contours(cntImgU,cnt_thresh)
+        self.contoursU = cntsU; self.contour_hierarchyU = hierU
+
         _cnts, limits, ellipses, pts, centers = self.extract_contour_x_bounds()
+
+        self.nObs = len(self.disparityBounds)
+        self.obsImgs = []
+        disp = np.copy(self.img)
+        self.display_obstacles = cv2.cvtColor(disp,cv2.COLOR_GRAY2BGR)
+        for i in range(self.nObs):
+            tmp = np.copy(self.img)
+            tmp = cv2.cvtColor(tmp,cv2.COLOR_GRAY2BGR)
+            try:
+                xs = self.xBounds[i]
+                ys,disp = self.find_obstacles(self.greyV,self.disparityBounds[i], _threshold=30, lower_threshold=1, verbose = True, display=self.greyV)
+                cv2.rectangle(tmp,(xs[0],ys[0]),(xs[1],ys[-1]),(150,0,0),1)
+                cv2.rectangle(self.display_obstacles,(xs[0],ys[0]),(xs[1],ys[-1]),(150,0,0),1)
+            except: pass
+            self.obsImgs.append(tmp)
+
+        self.plot_image(self.display_obstacles,2)
 
         if(input_method is 1):
             inputU = self.dilatedU
@@ -120,16 +157,129 @@ class UVMappingPipeline:
         dispU = self.line_finder(inputU, rho, ang, houghThresh, minLineLength, maxLineGap, line_method)
         dispV = self.line_finder(inputV, rho, ang, houghThresh, minLineLength, maxLineGap, line_method)
 
-        comp_imgs = [self.vmap,self.greyV,self.dilatedV,self.blurredV,self.closingV,dispV]
+        # comp_imgs = [self.vmap,self.greyV,self.dilatedV,self.blurredV,self.closingV,dispV]
+        comp_imgs = [self.umap,self.greyU,self.dilatedU,self.blurredU,self.closingU,dispU]
         composite = self.construct_composite_img(comp_imgs,2,3)
         self.plot_image(composite,3)
+
+    """
+    ============================================================================
+        Attempt to find the y boundaries for potential obstacles found from
+        the U-Map
+    ============================================================================
+    """
+    def find_obstacles(self, _img, disparityRange, _threshold=30, lower_threshold=1, draw_windows=True, verbose=False, timing=False, display=None):
+        if(timing): t0 = time.time()
+        # Temporary dump variables
+        count = 0; winCount = 0
+        maxStep = 40
+        flag_done = False
+        flag_found_start = False
+        good_inds = [] # Indices used for keeping track of pixels w/in current window
+        mean_pxls = []
+        ys = []
+
+        h,w = _img.shape[:2]
+
+        try: imgV = cv2.cvtColor(_img,cv2.COLOR_GRAY2BGR)
+        except: imgV = np.copy(_img)
+        # Keep a copy of the original input image for overlaying windows
+        try:
+            d = display.dtype
+            display_windows = np.copy(display)
+        except:
+            display_windows = np.copy(imgV)
+
+        tmp = np.copy(imgV)
+
+        # Prevent initial search coordinates from clipping search window at edges
+        xk = (disparityRange[1] + disparityRange[0]) / 2
+        yk = 0
+        dWy = 20
+        dWx = abs(xk - disparityRange[0])
+        if(dWx <= 0): dWx = 1
+
+        if(yk <= 0): yk = 0 + dWy
+        if(verbose): print("Starting Location: ", xk, yk, dWx)
+
+        # Grab all nonzero pixels
+        nonzero = tmp.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+
+        # Begin Sliding Window Search Technique
+        while(winCount <= maxStep and not flag_done):
+            # Store previously found indices as a fallback in case next window is insufficient
+            prev_good_inds = good_inds
+
+            # If we are at image width we must stop
+            if(xk >= w):
+                flag_done = True
+                if(verbose): print("Exiting: Reached max image width.")
+
+            # Slide window from previousy found center (Clip at image edges)
+            # Update vertical [Y] window edges
+            if(yk - dWy >= 0): wy_low = yk - dWy
+            else: wy_low = 0
+
+            if(yk + dWy <= h): wy_high = yk + dWy
+            else: wy_high = h
+
+            # Update horizontal [X] window edges
+            if(xk - dWx >= 0): wx_low = xk - dWx
+            else: wx_low = 0
+
+            if(xk + dWx <= w): wx_high = xk + dWx
+            else: wx_high = w
+
+            if(draw_windows):
+                cv2.circle(display_windows,(xk,yk),2,(255,0,255),-1)
+                cv2.rectangle(display_windows,(wx_low,wy_high),(wx_high,wy_low),(255,255,0), 1)
+
+            # Identify the nonzero pixels in x and y within the window
+            good_inds = ((nonzeroy >= wy_low) & (nonzeroy < wy_high) &
+                        (nonzerox >= wx_low) &  (nonzerox < wx_high)).nonzero()[0]
+            nPxls = len(good_inds)
+            if verbose == True:
+                print("Current Window [" + str(count) + "] Center: " + str(xk) + ", " + str(yk))
+                # print("\tCurrent Window X Limits: " + str(wx_low) + ", " + str(wx_high))
+                # print("\tCurrent Window Y Limits: " + str(wy_low) + ", " + str(wy_high))
+                print("\tCurrent Window # Good Pixels: " + str(nPxls))
+
+            # Record mean coordinates of pixels in window and update new window center
+            if(nPxls >= _threshold):
+                xmean = np.int(np.mean(nonzerox[good_inds]))
+                ymean = np.int(np.mean(nonzeroy[good_inds]))
+                mean_pxls.append([xmean,ymean])
+                if(count == 0): ys.append(yk - dWy)
+                else: ys.append(yk)
+                flag_found_start = True
+                count+=1
+                if(draw_windows): cv2.circle(display_windows,(xmean,ymean),2,(0,0,255),-1)
+            elif(nPxls <= lower_threshold and flag_found_start):
+                flag_done = True
+                ys.append(yk)
+
+            # Update New window center coordinates
+            xk = xk
+            yk = yk + 2*dWy
+            winCount += 1
+
+        if(draw_windows): self.plot_image(display_windows,9)
+
+        if(timing):
+            t1 = time.time()
+            dt = t1 - t0
+            print("\t[get_vmap_mask] --- Took %f seconds to complete" % (dt))
+        return ys, display_windows
+
 
     """
     ============================================================================
         Pre-process the UV-Maps before being used to detect obstacles
     ============================================================================
     """
-    def filter_image(self, grey_threshold, _e1, _e2, verbose=False, timing=False):
+    def filter_image(self, grey_thresholdU, grey_thresholdV, _e1, _e2, verbose=False, timing=False):
         if(timing): t0 = time.time()
         # Morphological Kernels
         kernelU = np.ones((_e1,_e2),np.uint8)
@@ -138,7 +288,16 @@ class UVMappingPipeline:
         # U-Map: Disparity Filtering
         tmpU = np.copy(self.umap)
         tmpU = cv2.cvtColor(tmpU,cv2.COLOR_GRAY2BGR)
-        _, greyU = cv2.threshold(tmpU,grey_threshold,255,cv2.THRESH_BINARY)
+        # Filter out disparities extremely close
+        tmpH,tmpW = tmpU.shape[:2]
+        black = np.zeros((tmpH,tmpW,3),dtype=np.uint8)
+        cv2.rectangle(black,(0,0),(tmpW,self.deadzone_ud),(255,255,255), cv2.FILLED)
+        cv2.rectangle(black,(0,tmpH-self.deadzone_ud),(tmpW,tmpH),(255,255,255), cv2.FILLED)
+        maskU = cv2.cvtColor(black,cv2.COLOR_BGR2GRAY)
+        maskU_inv = cv2.bitwise_not(maskU)
+        resU = cv2.bitwise_and(tmpU, tmpU, mask = maskU_inv)
+
+        _, greyU = cv2.threshold(resU,grey_thresholdU,255,cv2.THRESH_BINARY)
 
         dilation_U = cv2.dilate(greyU,kernelU,iterations = 1)
         blur_U = cv2.GaussianBlur(dilation_U,(5,5),0)
@@ -147,12 +306,12 @@ class UVMappingPipeline:
         # V-Map: Disparity Filtering w/ siliding window filter
         tmpV = np.copy(self.vmap)
         tmpV = cv2.cvtColor(tmpV,cv2.COLOR_GRAY2BGR)
-        # _, greyV = cv2.threshold(tmpV,grey_threshold,255,cv2.THRESH_BINARY)
+        _, greyV = cv2.threshold(tmpV,grey_thresholdV,255,cv2.THRESH_BINARY)
 
         # Sliding Window Technique for filtering out ground
-        maskV, invMaskV = self.get_vmap_mask(tmpV)
-        res = cv2.bitwise_and(tmpV, tmpV, mask = invMaskV)
-        _, greyV = cv2.threshold(res,grey_threshold,255,cv2.THRESH_BINARY)
+        maskV, invMaskV = self.get_vmap_mask(greyV,verbose=False)
+        resV = cv2.bitwise_and(tmpV, tmpV, mask = invMaskV)
+        _, greyV = cv2.threshold(resV,grey_thresholdV,255,cv2.THRESH_BINARY)
 
         dilation_V = cv2.dilate(greyV,kernelV,iterations = 1)
         blur_V = cv2.GaussianBlur(dilation_V,(5,5),0)
@@ -164,7 +323,7 @@ class UVMappingPipeline:
         self.blurredU = np.copy(blur_U)
         self.closingU = np.copy(closing_U)
 
-        self.vMasked = np.copy(res)
+        self.vMasked = np.copy(resV)
         self.greyV = np.copy(greyV)
         self.dilatedV = np.copy(dilation_V)
         self.blurredV = np.copy(blur_V)
@@ -183,7 +342,9 @@ class UVMappingPipeline:
         Attempt to find all the contours from input image w/ area filtering
     ============================================================================
     """
-    def find_contours(self, _img, _threshold=0, verbose=True, timing=True):
+    def find_contours(self, _img, _threshold=0, verbose=True, timing=True, use_areas=False):
+        areas = []; perimeters = []
+        filtered_areas = []; filtered_perimeters = []
         if(timing): t0 = time.time()
 
         try: img = cv2.cvtColor(_img,cv2.COLOR_BGR2GRAY)
@@ -192,16 +353,25 @@ class UVMappingPipeline:
 
         _, contours, hierarchy = cv2.findContours(img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
         areas = [cv2.contourArea(cnt) for cnt in contours]
+        perimeters = [cv2.arcLength(cnt,True) for cnt in contours]
 
-        filtered_cnts = [cnt for cnt in contours if cv2.contourArea(cnt) >= _threshold]
-        filtered_areas = [cv2.contourArea(cnt) for cnt in filtered_cnts]
+        if(use_areas):
+            filtered_cnts = [cnt for cnt in contours if cv2.contourArea(cnt) >= _threshold]
+            filtered_areas = [cv2.contourArea(cnt) for cnt in filtered_cnts]
+        else:
+            filtered_cnts = [cnt for cnt in contours if cv2.arcLength(cnt,True) >= _threshold]
+            filtered_perimeters = [cv2.arcLength(cnt,True) for cnt in filtered_cnts]
 
         self.contours = filtered_cnts
         self.contour_hierarchy = hierarchy
 
         if(verbose):
-            print("Raw Contour Areas:",areas)
-            print("Filtered Contour Areas:",filtered_areas)
+            if(use_areas):
+                print("Raw Contour Areas:",areas)
+                print("Filtered Contour Areas:",filtered_areas)
+            else:
+                print("Raw Contour Perimeters:",perimeters)
+                print("Filtered Contour Perimeters:",filtered_perimeters)
 
         if(timing):
             t1 = time.time()
@@ -215,34 +385,49 @@ class UVMappingPipeline:
         Attempt to find the horizontal bounds for detected contours
     ============================================================================
     """
-    def extract_contour_x_bounds(self, _display=None, verbose=False, timing=True):
-        if(_display is None): display = np.copy(self.greyV)
+    def extract_contour_x_bounds(self, _cnts=None, _display=None, verbose=False, timing=True):
+        if(_display is None): display = np.copy(self.greyU)
         else: display = np.copy(_display)
 
-        if(timing): t0 = time.time()
+        if(_cnts is None): cnts = self.contoursU
+        else: cnts = _cnts
+
         limits = []; ellipses = []; pts = []; centers = []
-        cnts = self.contours
+        self.xBounds = []
+        self.disparityBounds = []
+
+        # print(x, x+w, y, y+h)
+
+        if(timing): t0 = time.time()
 
         for cnt in cnts:
-            ellipse = cv2.fitEllipse(cnt)
-            cx, cy = np.int32(ellipse[0])
-            h, w = np.int32(ellipse[1])
-            dx,dy = w/2,h/2
+            try:
+                ellipse = cv2.fitEllipse(cnt)
+                cx, cy = np.int32(ellipse[0])
+                h, w = np.int32(ellipse[1])
+                dx,dy = w/2,h/2
 
-            if(verbose):
-                print("Center: (%d, %d)" % (cx,cy))
-                print("H, W: (%d, %d)" % (dx,dy))
+                if(verbose):
+                    print("Center: (%d, %d)" % (cx,cy))
+                    print("H, W: (%d, %d)" % (dx,dy))
 
-            xr = cx + dx; xl = cx - dx
-            yt = cy - dy; yb = cy + dy
-            ptL = (xl,cy); ptR = (xr,cy)
+                xr = cx + dx; xl = cx - dx
+                yt = cy - dy; yb = cy + dy
+                ptL = (xl,cy); ptR = (xr,cy)
 
-            lim = np.array([[xl,xr],[yt,yb]])
-            limits.append(lim)
+                lim = np.array([[xl,xr],[yt,yb]])
+                limits.append(lim)
 
-            pts.append([ptL, ptR])
-            ellipses.append(ellipse)
-            centers.append((cx,cy))
+                pts.append([ptL, ptR])
+                ellipses.append(ellipse)
+                centers.append((cx,cy))
+            except: pass
+
+            try:
+                x,y,rectw,recth = cv2.boundingRect(cnt)
+                self.xBounds.append([x, x + rectw])
+                self.disparityBounds.append([y, y + recth])
+            except: pass
 
         if(timing):
             t1 = time.time()
@@ -267,11 +452,12 @@ class UVMappingPipeline:
 
         for i, cnt in enumerate(_cnts):
             cv2.drawContours(tmp, [cnt], 0, (255,0,0), 2)
-            cv2.ellipse(display,ellipses[i],(0,255,0),2)
-            cv2.circle(display,centers[i],2,(255,0,255), 5)
-            cv2.circle(display,pts[i][0],2,(0,255,255), 5)
-            cv2.circle(display,pts[i][1],2,(0,255,255), 5)
-
+            try:
+                cv2.ellipse(display,ellipses[i],(0,255,0),2)
+                cv2.circle(display,centers[i],2,(255,0,255), 5)
+                cv2.circle(display,pts[i][0],2,(0,255,255), 5)
+                cv2.circle(display,pts[i][1],2,(0,255,255), 5)
+            except: pass
         sborder = np.ones((_img.shape[0],5,3),dtype=np.uint8)
         sborder[np.where((sborder==[1,1,1]).all(axis=2))] = [255,0,255]
         helper = np.concatenate((
@@ -292,7 +478,7 @@ class UVMappingPipeline:
         display = None
         count = 0
         maxStep = 40
-        deadzone_x = 5
+        deadzone_x = self.deadzone_vd
         flag_done = False
         good_inds = [] # Indices used for keeping track of pixels w/in current window
         mean_pxls = []
@@ -401,6 +587,7 @@ class UVMappingPipeline:
 
         if(draw_windows): self.plot_image(display_windows,1)
 
+        cv2.rectangle(black,(0,0),(deadzone_x,h),(255,255,255), cv2.FILLED)
         mask = cv2.cvtColor(black,cv2.COLOR_BGR2GRAY)
         mask_inv = cv2.bitwise_not(mask)
 
