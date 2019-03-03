9a60fa15e64f28f2003f9bb8af1744dd5bb590d9
Hunter Young <hunter.lw.young@gmail.com>
2019-03-02 21:26:10 -0500

    working on cam tools

--- tools/r200_camera.py
+++ tools/r200_camera.py
@@ -1,106 +1,87 @@
-import cv2, time
 import numpy as np
-import pyrealsense as rs
+import cv2, time, pprint
+import pyrealsense as pyrs
+from pyrealsense.constants import rs_option
 import matplotlib.pyplot as plt
 
-import types
-from collections import Mapping, Set, Sequence
-import pprint
-rs.constants.rs_format.RS_FORMAT_Z16
 pp = pprint.PrettyPrinter(indent=4)
-# dual python 2/3 compatability, inspired by the "six" library
-string_types = (str, unicode) if str is bytes else (str, bytes)
-iteritems = lambda mapping: getattr(mapping, 'iteritems', mapping.items)()
 
-def objwalk(obj, path=(), memo=None):
-    if memo is None:
-        memo = set()
-    iterator = None
-    if isinstance(obj, Mapping):
-        iterator = iteritems
-    elif isinstance(obj, (Sequence, Set)) and not isinstance(obj, string_types):
-        iterator = enumerate
-    if iterator:
-        if id(obj) not in memo:
-            memo.add(id(obj))
-            for path_component, value in iterator(obj):
-                for result in objwalk(value, path + (path_component,), memo):
-                    yield result
-            memo.remove(id(obj))
-    else:
-        yield path, obj
 
-def dump_obj(obj, level=0):
-    for key, value in obj.__dict__.items():
-        if not isinstance(value, types.InstanceType):
-             print " " * level + "%s -> %s" % (key, value)
+class CameraR200(object):
+    def __init__(self):
+        self.flag_save = 0
+        self.rgb_img = None
+        self.depth_img = None
+
+        # Attempt to establish camera service
+        try: self.srv = pyrs.Service()
+        except:
+            self.srv = None
+            print("[ERROR] Could not establish CameraR200 Service!")
+
+        # Attempt to connect to camera device
+        try: self.dev = self.srv.Device(depth_control_preset=1)
+        except:
+            self.dev = None
+            print("[ERROR] Could not establish CameraR200 Device!")
+        # Attempt to configure camera settings
+        self.apply_configuration()
+
+        if(self.flag_save): # Configure depth and color streams
+            fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+            self.writerD = cv2.VideoWriter("realsense_depth.avi", fourcc, 60,(640, 480), True)
+            self.writerRGB = cv2.VideoWriter("realsense_rgb.avi", fourcc, 60,(640, 480), True)
         else:
-            dump_obj(value, level + 2)
+            self.writerD = None
+            self.writerRGB = None
 
-flag_save = 0
+    def __del__(self):
+        print("[INFO] Closing CameraR200 object")
+        if(self.dev is not None): self.dev.stop()
+        if(self.srv is not None): self.srv.stop()
+        if(self.writerD is not None): self.writerD.release()
+        if(self.writerRGB is not None): self.writerRGB.release()
 
-# Configure depth and color streams
-if flag_save:
-    fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
-    writerRGB = cv2.VideoWriter("realsense_rgb.avi", fourcc, 60,(640, 480), True)
-    writerD = cv2.VideoWriter("realsense_depth.avi", fourcc, 60,(640, 480), True)
+    def close(self):
+        self.__del__()
 
-serv = rs.Service()
-# dev = serv.Device(rs.constants.rs_ivcam_preset.RS_IVCAM_PRESET_SHORT_RANGE)
-pp.pprint(serv)
+    def apply_configuration(self, lr_exposure=100.0, lr_gain=137.0, ivcam_preset=0):
+        try:  # set custom gain/exposure values to obtain good depth image
+            custom_options = [(rs_option.RS_OPTION_R200_LR_EXPOSURE, lr_exposure),
+                              (rs_option.RS_OPTION_R200_LR_GAIN, lr_gain)]
+            self.dev.set_device_options(*zip(*custom_options))
+            self.dev.apply_ivcam_preset(ivcam_preset)
+        except pyrs.RealsenseError:
+            print("[ERROR] CameraR200 --- Could not apply configuration")
+            pass
 
-p = objwalk(serv)
-print(p)
-print("\r\n\r\n")
-# print(o)
-# config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-# config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-#
-# # Start streaming
-# profile = pipeline.start(config)
-# # time.sleep(2.0)
-#
-# try:
-#     while True:
-#
-#         # Wait for a coherent pair of frames: depth and color
-#         frames = pipeline.wait_for_frames()
-#         depth_frame = frames.get_depth_frame()
-#         color_frame = frames.get_color_frame()
-#         if not depth_frame or not color_frame:
-#             continue
-#
-#         # Convert images to numpy arrays
-#         color_image = np.asanyarray(color_frame.get_data())
-#         depth_image = np.asanyarray(depth_frame.get_data())
-#         # norm_depth_image = cv2.normalize(depth_image, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-#         # norm_depth_image = cv2.cvtColor(norm_depth_image, cv2.COLOR_GRAY2RGB)
-#         norm_depth_image = cv2.convertScaleAbs(depth_image, alpha=0.03)
-#         # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-#         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-#
-#         # Stack both images horizontally
-#         images = np.hstack((color_image, depth_colormap))
-#         depth_scale = profile.get_device().first_depth_sensor().get_depth_scale()
-#         # print("Depth Scale: %f" % depth_scale)
-#         # Show images
-#         dImg = depth_image*depth_scale
-#         dRgb = color_image
-#
-#         if flag_save:
-#             writerRGB.write(dRgb)
-#             writerD.write(depth_colormap)
-#
-#         cv2.namedWindow('Disparity', cv2.WINDOW_NORMAL)
-#         cv2.namedWindow('RealSense', cv2.WINDOW_NORMAL)
-#         cv2.imshow('RealSense', images)
-#         cv2.imshow('Disparity', norm_depth_image)
-#         cv2.waitKey(1)
-#
-# finally:
-#
-#     # Stop streaming
-#     pipeline.stop()
-#     if flag_save:
-#         writerRGB.release()
-#         writerD.release()
+    def get_intrinsics(self, verbose=False):
+        intr = self.dev.depth_intrinsics
+
+        fx = intr.fx
+        fy = intr.fy
+        dscale = self.dev.depth_scale
+        if(verbose): print(dscale)
+
+        return intr
+
+    def get_extrinsics(self, stream_range=(2,3), verbose=False):
+        extr = self.dev.get_device_extrinsics(stream_range[0],stream_range[1])
+        baseline = extr.translation[0]
+        if(verbose): print(baseline)
+
+        return extr, baseline
+
+    def read(self):
+        self.dev.wait_for_frames()
+        rgb = self.get_rgb_image()
+        depth = self.get_depth_image()
+        return rgb, depth
+
+    def get_rgb_image(self):
+        img = self.dev.color
+        return img
+
+    def get_depth_image(self):
+        img = self.dev.depth
+        return img
